c903784e004b9532952c374ed2139a3fb6ab2d9a
Nou <cliff.foster@idi-systems.com>
2014-11-29 19:22:46 -0800

    JNET loader

--- addons/main/RscDisplayMultiplayerSetup.hpp
+++ addons/main/RscDisplayMultiplayerSetup.hpp
@@ -1,4 +1,5 @@
 class RscDisplayMultiplayerSetup : RscStandardDisplay
 {
 	onLoad = QUOTE(DEF_RscDisplayMultiplayerSetupLoader);
+	onMouseMoving = "if((uiNamespace getVariable ['JNET_mpSetupInit', false]) && {scriptDone (uiNamespace getVariable 'JNET_checkScriptId')}) then { ['RscDisplayMultiplayerSetup'] call (uiNamespace getVariable 'jnet_fnc_eventProcessor'); uiNamespace setVariable ['JNET_mpSetupInit', false]; };";
 };
--- addons/main/RscDisplayRemoteMissions.hpp
+++ addons/main/RscDisplayRemoteMissions.hpp
@@ -1,4 +1,5 @@
 class RscDisplayRemoteMissions: RscStandardDisplay
 {
 	onLoad = QUOTE(DEF_RscDisplayRemoteMissionsLoader);
+	onMouseMoving = "if((uiNamespace getVariable ['JNET_rmInit', false]) && {scriptDone (uiNamespace getVariable 'JNET_checkScriptId')}) then { ['RscDisplayRemoteMissions'] call (uiNamespace getVariable 'jnet_fnc_eventProcessor'); uiNamespace setVariable ['JNET_rmInit', false]; };";
 };
